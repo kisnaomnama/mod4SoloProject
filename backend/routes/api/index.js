b617b9f4252e617edd7e040007cc22536d5bc699
Krishna <kisnasur@gmail.com>
2024-02-23 10:02:23 -0800

    Add User logout backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,10 +4,18 @@ const router = require('express').Router();
 // GET /api/set-token-cookie
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 // router.get( '/restore-user', (req, res) => {
 //     return res.json(req.user);
