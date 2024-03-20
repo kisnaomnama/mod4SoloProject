d7b211bb93bc06b4b1a6b5e059eaf0427c5e8311
Krishna <kisnasur@gmail.com>
2024-03-20 03:59:29 -0700

    images added

--- backend/db/seeders/20240226235405-demo-spot-image.js
+++ backend/db/seeders/20240226235405-demo-spot-image.js
@@ -12,37 +12,37 @@ module.exports = {
     const spotImagesData = [
       {
         spotId: 1,
-        url: 'https://example.com/image1.jpg',
+        url: 'https://res.cloudinary.com/dsnllj445/image/upload/v1710930147/samples/houses/pexels-pixabay-259588_vovazn.jpg',
         preview: true 
       },
       {
         spotId: 2,
-        url: 'https://example.com/image2.jpg',
-        preview: false
+        url: 'https://res.cloudinary.com/dsnllj445/image/upload/v1710930083/samples/houses/istockphoto-576909072-1024x1024_gse5pz.jpg',
+        preview: true
       },
       {
         spotId: 3,
-        url: 'https://example.com/image3.jpg',
+        url: 'https://res.cloudinary.com/dsnllj445/image/upload/v1710930058/samples/houses/pexels-binyamin-mellish-1396122_dshka2.jpg',
         preview: true
       },
       {
         spotId: 2,
-        url: 'https://example.com/image4.jpg',
+        url: 'https://res.cloudinary.com/dsnllj445/image/upload/v1710929858/samples/houses/istockphoto-1264323513-1024x1024_cvxpoe.jpg',
         preview: true
       },
       {
         spotId: 2,
-        url: 'https://example.com/image5.jpg',
+        url: 'https://res.cloudinary.com/dsnllj445/image/upload/v1710929819/samples/houses/istockphoto-1208210864-1024x1024_ew9jp6.jpg',
         preview: true
       },
       {
         spotId: 4,
-        url: 'https://example.com/image6.jpg',
+        url: 'https://res.cloudinary.com/dsnllj445/image/upload/v1710929945/samples/houses/istockphoto-1349313772-1024x1024_e3tpz5.jpg',
         preview: true
       },
       {
         spotId: 5,
-        url: 'https://example.com/image7.jpg',
+        url: 'https://res.cloudinary.com/dsnllj445/image/upload/v1710929843/samples/houses/istockphoto-1255835529-1024x1024_hmgzpz.jpg',
         preview: true
       }
     ];
